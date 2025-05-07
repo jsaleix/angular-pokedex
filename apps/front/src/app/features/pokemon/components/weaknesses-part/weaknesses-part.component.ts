95dd4824cb480a23462d71c5384e5c597faa3202
julian-saleix <julian.saleix@gmail.com>
2025-05-07 18:02:10 +0200

    fix: weaknessesPart expression changed after checked error

--- apps/front/src/app/features/pokemon/components/weaknesses-part/weaknesses-part.component.ts
+++ apps/front/src/app/features/pokemon/components/weaknesses-part/weaknesses-part.component.ts
@@ -12,12 +12,13 @@ import {
 })
 export class WeaknessesPartComponent {
   types = input<string[]>([]);
+  
   weaknesses = computed<Effectiveness>(() => {
     const values = this.types();
     if (values.length === 0) return { x0: [], 'x0.5': [], x2: [] };
     else return calcWeaknesses(values);
   });
-  weaknessesToArray = computed(() => Object.entries(this.weaknesses()));
+  weaknessesToArray = computed(() => Object.entries(this.weaknesses()).reverse());
   noWeaknesses = computed(() => {
     const weaknesses = this.weaknesses();
     const values = Object.values(weaknesses);
