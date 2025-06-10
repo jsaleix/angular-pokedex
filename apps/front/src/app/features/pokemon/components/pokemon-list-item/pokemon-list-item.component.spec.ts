e0be880e20a7079710c1eda8ef8ecc42e283dbcf
julian-saleix <julian.saleix@gmail.com>
2025-06-10 16:43:14 +0200

    Fix: PkmListItem should create test

--- apps/front/src/app/features/pokemon/components/pokemon-list-item/pokemon-list-item.component.spec.ts
+++ apps/front/src/app/features/pokemon/components/pokemon-list-item/pokemon-list-item.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonListItemComponent } from './pokemon-list-item.component';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonInListI } from '@features/pokemon/types/api';
+
+class ActivatedRouteMock {
+  queryParams = new Observable((observer) => {
+    const urlParams = {};
+
+    observer.next(urlParams);
+    observer.complete();
+  });
+}
+
+const MOCK_PKM = {
+  name: 'Charizard',
+  url: 'https://pokeapi.co/api/v2/pokemon/6/',
+} satisfies PokemonInListI;
 
 describe('PokemonListItemComponent', () => {
   let component: PokemonListItemComponent;
@@ -9,10 +26,12 @@ describe('PokemonListItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PokemonListItemComponent],
+      providers: [{ provide: ActivatedRoute, useClass: ActivatedRouteMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonListItemComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('pokemon', MOCK_PKM);
     fixture.detectChanges();
   });
 
