4b263b2ee0ab8d8ed6aa7f16d273a8907c42f249
julian-saleix <julian.saleix@gmail.com>
2025-06-10 16:57:58 +0200

    Fix: Pokedex page should create test

--- apps/front/src/app/features/pokemon/pages/pokedex/pokedex.component.spec.ts
+++ apps/front/src/app/features/pokemon/pages/pokedex/pokedex.component.spec.ts
@@ -1,14 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { PokedexComponent } from './pokedex.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PokemonService } from '@features/pokemon/services/pokemon.service';
 
 describe('PokedexComponent', () => {
   let component: PokedexComponent;
   let fixture: ComponentFixture<PokedexComponent>;
+  let mockPokemonService: jasmine.SpyObj<PokemonService>;
+  let mockRouter: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    mockPokemonService = jasmine.createSpyObj('PokemonService', [
+      'getPokemons',
+    ]);
+    mockPokemonService.getPokemons.and.returnValue(
+      of({ count: 0, next: null, previous: null, results: [] }),
+    );
+
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
       imports: [PokedexComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              queryParamMap: {
+                get: (key: string) => {
+                  if (key === 'offset') return '0';
+                  return null;
+                },
+              },
+            },
+          },
+        },
+        { provide: PokemonService, useValue: mockPokemonService },
+        { provide: Router, useValue: mockRouter },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokedexComponent);
