d0613f3cab09866b67eb49dfd96d4d0469f1d097
julian-saleix <julian.saleix@gmail.com>
2025-06-10 17:08:43 +0200

    Fix: SearchPkm should create test

--- apps/front/src/app/features/pokemon/components/search-pkm/search-pkm.component.spec.ts
+++ apps/front/src/app/features/pokemon/components/search-pkm/search-pkm.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchPkmComponent } from './search-pkm.component';
+import { HttpClient } from '@angular/common/http';
+import { PokemonDataService } from '@features/pokemon/services/pokemon-data.service';
+import { of } from 'rxjs';
+import { PokemonData } from '@features/pokemon/types/data';
 
 describe('SearchPkmComponent', () => {
   let component: SearchPkmComponent;
   let fixture: ComponentFixture<SearchPkmComponent>;
+  let mockPkmDataService: jasmine.SpyObj<PokemonDataService>;
 
   beforeEach(async () => {
+    mockPkmDataService = jasmine.createSpyObj('PkmDataService', [
+      'searchByName',
+    ]);
+    mockPkmDataService.searchByName.and.returnValue([]);
+
     await TestBed.configureTestingModule({
-      imports: [SearchPkmComponent]
-    })
-    .compileComponents();
+      imports: [SearchPkmComponent],
+      providers: [
+        { provide: PokemonDataService, useValue: mockPkmDataService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchPkmComponent);
     component = fixture.componentInstance;
