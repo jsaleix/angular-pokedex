2dcb5a3d4867554431804632365fbc9dbd48c474
julian-saleix <julian.saleix@gmail.com>
2025-06-12 16:13:40 +0200

    chore: adding anim + adding it in pkmCard

--- apps/front/src/app/features/pokemon/components/pokemon-card/pokemon-card.component.ts
+++ apps/front/src/app/features/pokemon/components/pokemon-card/pokemon-card.component.ts
@@ -6,6 +6,7 @@ import { PokemonI, PokemonSpeciesI } from '@features/pokemon/types/api';
 import { SquareIconComponent } from '@shared/components/icons/square-icon/square-icon.component';
 import { StarIconComponent } from '@shared/components/icons/star-icon/star-icon.component';
 import { TriangleIconComponent } from '@shared/components/icons/triangle-icon/triangle-icon.component';
+import { TypeItemComponent } from '../type-item/type-item.component';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -14,6 +15,7 @@ import { TriangleIconComponent } from '@shared/components/icons/triangle-icon/tr
     StarIconComponent,
     SquareIconComponent,
     UpperCasePipe,
+    TypeItemComponent
   ],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css',
