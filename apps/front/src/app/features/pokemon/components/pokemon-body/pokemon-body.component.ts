52bb0490a7a4c1fc4d4c1c0f6990f2a765091d8f
julian-saleix <julian.saleix@gmail.com>
2025-06-16 16:01:52 +0200

    feat: can switch pokemon to next or previous one. #7

--- apps/front/src/app/features/pokemon/components/pokemon-body/pokemon-body.component.ts
+++ apps/front/src/app/features/pokemon/components/pokemon-body/pokemon-body.component.ts
@@ -3,10 +3,15 @@ import { PokemonDTO } from '@features/pokemon/models/pokemon.dto';
 import { SpeciesDTO } from '@features/pokemon/models/species.dto';
 import { WeaknessesPartComponent } from '../weaknesses-part/weaknesses-part.component';
 import { PokemonBodySelectorComponent } from '../pokemon-body-selector/pokemon-body-selector.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-body',
-  imports: [WeaknessesPartComponent, PokemonBodySelectorComponent],
+  imports: [
+    WeaknessesPartComponent,
+    PokemonBodySelectorComponent,
+    RouterModule,
+  ],
   templateUrl: './pokemon-body.component.html',
   styleUrl: './pokemon-body.component.css',
 })
