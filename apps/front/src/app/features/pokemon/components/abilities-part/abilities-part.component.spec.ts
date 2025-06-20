4ba950b32a6a85107f29d62a67882f1a48ae8aa7
julian-saleix <julian.saleix@gmail.com>
2025-06-20 16:54:56 +0200

    fix: AbilitiesPart test should create fixed

--- apps/front/src/app/features/pokemon/components/abilities-part/abilities-part.component.spec.ts
+++ apps/front/src/app/features/pokemon/components/abilities-part/abilities-part.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AbilitiesPartComponent } from './abilities-part.component';
+import { mapAbilityFromApi } from '@features/pokemon/models/abilities.dto';
+import { mockAbilityData } from '@features/pokemon/services/data/abilities';
+
+const abilities = mapAbilityFromApi(mockAbilityData);
 
 describe('AbilitiesPartComponent', () => {
   let component: AbilitiesPartComponent;
@@ -8,12 +12,13 @@ describe('AbilitiesPartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AbilitiesPartComponent]
-    })
-    .compileComponents();
+      imports: [AbilitiesPartComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AbilitiesPartComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('abilities', [abilities]);
     fixture.detectChanges();
   });
 
